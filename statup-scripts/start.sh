ddce137a4f9ed514097ad42835f8f5086ba29cdd
Cerial <71380797+CerialPvP@users.noreply.github.com>
2022-09-05 22:57:39 +0300

    Create start.sh

---
+++ statup-scripts/start.sh
@@ -0,0 +1,8 @@
+#!bin/bash
+echo "|------------------------------|"
+echo "|  Automated Minecraft Server  |"
+echo "|      Made by CerialPvP       |"
+echo "|    CerialPvP/AutoMCServer    |"
+echo "|------------------------------|"
+
+java -Xmx2G -Xms2G -jar paper.jar
